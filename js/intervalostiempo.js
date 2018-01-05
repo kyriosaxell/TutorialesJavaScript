4033545c350f72d2b481bdee982235aae8c1b253
Axell <soad.dvm@hotmail.com>
2018-01-04 22:36:53 -0600

    Tercer commit

--- js/intervalostiempo.js
+++ js/intervalostiempo.js
@@ -2,7 +2,14 @@ var tiempo = document.querySelector("#tiempo");
 var segundos = 0;
 
 //INTERVALO DE TIEMPO
-setInterval(function(){
+var intervalo = setInterval(function(){
   segundos++;
   tiempo.innerHTML = segundos;
 }, 1000)
+
+//SET TIMEOUT(RETARDO DE TIEMPO)
+
+setTimeout(function() {
+  alert("Se cumplió el tiempo")
+  clearInterval(intervalo); //"detieme"
+},5000)
