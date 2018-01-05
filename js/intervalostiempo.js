614356c1606b2db185b970b767200789ae3dee66
Axell <soad.dvm@hotmail.com>
2018-01-04 22:04:31 -0600

    segundo commit

--- js/intervalostiempo.js
+++ js/intervalostiempo.js
@@ -2,4 +2,7 @@ var tiempo = document.querySelector("#tiempo");
 var segundos = 0;
 
 //INTERVALO DE TIEMPO
-setInterval()
+setInterval(function(){
+  segundos++;
+  tiempo.innerHTML = segundos;
+}, 1000)
